cdc97de4260f81dc734b4d594a6543832efc96d1
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-12-21 10:15:03 +1300

    Style SoloParticipantMatchInfo so images are all lined horizontally.

--- code/game-fights/src/components/match_info/SoloParticipantInfo.jsx
+++ code/game-fights/src/components/match_info/SoloParticipantInfo.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
+import './../../style/SoloParticipantInfo.css'
+
 const SoloParticipantInfo = props => {
   return (
     <div>
       {/* Render the image of the Judge */}
-      <div id='judgeDisplay'>
+      <div className='userDisplay'>
         <img src='https://via.placeholder.com/100' alt='Judge' />
         <p>Judge</p>
       </div>
 
       {/* Render the images of the participants. */}
-      <div id='participantsDisplay'>
+      <div className='userDisplay'>
         {props.users.map(element => {
           // Find image based on user - IMPLEMENT LATER
           // For now, just use a placeholder image.
