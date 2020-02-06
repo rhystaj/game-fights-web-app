b92ab066c0bc7d45dd0498dca3e792bbf238b64a
rhystaj <rhystaj@gmail.com>
2020-02-07 11:20:54 +1300

    Create automatically updating component for viewing questions.

--- code/game-fights/src/backend_interface/mock_interface/interface.js
+++ code/game-fights/src/backend_interface/mock_interface/interface.js
@@ -5,12 +5,12 @@ import submissions from './test_data/submissions'
 import matchData from './test_data/matchData'
 import questions from './test_data/questions'
 
-export default Object.freeze({
+const beInterface = {
     
     queryUserMatchStatus: function(queryCallback){
         
         //Set timeout is to simulate latency.
-        setTimeout(() => {queryCallback(status.JUDGING);}, 1000);
+        setTimeout(() => {queryCallback(status.PARTCIPATING);}, 1000);
         
     },
 
@@ -20,17 +20,26 @@ export default Object.freeze({
 
     queryQuestions: function(queryCallback){
         setTimeout(() => {queryCallback({questions});}, 1000);
+        
+        //To simulate a question being added after the page has loaded.
+        setTimeout(() => {beInterface.updateQuestion()}, 10000);
+    },
+
+    updateQuestion: function(){
+        if(this.events.onQuestionUpdate !== undefined){
+            this.events.onQuestionUpdate(questions.concat("New quesiton"));
+        }
     },
 
     queryAnswerSubmissions: function(queryCallback){
         setTimeout(() => {queryCallback( {submissions} );}, 1000);
     },
 
-    queryMatchInfo(queryCallback){
+    queryMatchInfo: function(queryCallback){
         setTimeout(() => queryCallback(matchData.soloData), 1000);
     },
 
-    submitQuestion(data, successCallback, failureCallback){
+    submitQuestion: function(data, successCallback, failureCallback){
         
         if(data.localeCompare("Fail") === 0){
             //For testing purposes.
@@ -40,6 +49,12 @@ export default Object.freeze({
             successCallback();
         }
         
+    },
+
+    events: {
+        onQuestionUpdate: undefined //Will be overridden.
     }
 
-});
+}
+
+export default beInterface;
--- code/game-fights/src/components/match_management/ParticipantMatchManager.jsx
+++ code/game-fights/src/components/match_management/ParticipantMatchManager.jsx
@@ -1,9 +1,25 @@
+import React from 'react';
 import MatchManager from './MatchManager';
+
+import ParticipantMatchInfo from './match_info/ParticipantMatchInfo';
+import QuestionsViewer from './question_management/QuestionViewer'
+
+import matchState from './../../enums/matchStage';
 
 class ParticipantMatchManager extends MatchManager{
 
     constructor(props){
         super(props);
+    }
+
+    renderMatchInfo(){
+        return <ParticipantMatchInfo />
+    }
+
+    renderManagementComponent(stage){
+        if(stage === matchState.DETERMINING_QUESTIONS){
+            return<QuestionsViewer />
+        }
     }
 
 }
