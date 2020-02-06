72a474d650f2603a6ba26cbb5d610f4721567e0b
rhystaj <rhystaj@gmail.com>
2020-02-06 15:17:55 +1300

    Add test query for questions.

--- code/game-fights/src/backend_interface/mock_interface/interface.js
+++ code/game-fights/src/backend_interface/mock_interface/interface.js
@@ -1,17 +1,28 @@
 import status from './../../enums/userMatchStatus'
+import stage from './../../enums/matchStage'
+
 import submissions from './test_data/submissions'
 import matchData from './test_data/matchData'
+import questions from './test_data/questions'
 
 export default Object.freeze({
     
     queryUserMatchStatus: function(queryCallback){
         
         //Set timeout is to simulate latency.
-        setTimeout(() => {queryCallback(status.PARTCIPATING);}, 1000);
+        setTimeout(() => {queryCallback(status.JUDGING);}, 1000);
         
     },
 
-    queryQuestionSubmissions: function(queryCallback){
+    queryMatchStage: function(queryCallback){
+        setTimeout(() => {queryCallback(stage.DETERMINING_QUESTIONS)}, 1000);
+    },
+
+    queryQuestions: function(queryCallback){
+        setTimeout(() => {queryCallback({questions});}, 1000);
+    },
+
+    queryAnswerSubmissions: function(queryCallback){
         setTimeout(() => {queryCallback( {submissions} );}, 1000);
     },
 
