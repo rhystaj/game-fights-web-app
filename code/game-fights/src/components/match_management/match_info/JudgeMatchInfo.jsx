55e940fe11ca9cb06148aa936c74e4020b6ec079
rhystaj <rhystaj@gmail.com>
2020-02-07 16:41:13 +1300

    Create JudgeMatchInfo.jsx

---
+++ code/game-fights/src/components/match_management/match_info/JudgeMatchInfo.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import MatchInfo from './MatchInfo';
+import TextEntry from '../../utility/TextEntry';
+import beInterface from '../../../backend_interface/interface';
+
+import ParticipantsInviteScreen from '../../modals/ParticipantsInviteScreen';
+
+class JudgeMatchInfo extends MatchInfo{
+
+    constructor(props){
+        super(props);
+
+        this.state.editingTitle = false;
+    }
+
+    onConfirmTitle = title => {
+        beInterface.submitMatchTitle(title, this.onTitleSubmissionSuccess(title), )
+    }
+
+    onCancelTitle = () => {
+        this.setState({editingTitle: false});
+    }
+
+    onTitleSubmissionSuccess = title => () => {
+        this.setState({
+            title: title,
+            editingTitle: false
+        })
+    }
+
+    onTitleSubmissionFailure(){
+        //Not sure yet...
+    }
+
+    renderTitle(title){
+        if(this.state.editingTitle){
+            return(
+                <TextEntry 
+                    onConfirmEntry={this.onConfirmTitle}
+                    onCancelEntry={this.onCancelTitle}
+                />
+            )
+        }
+        else{
+            return <h1 onClick={() => this.setState({editingTitle: true})}>{title}</h1>
+        }
+    }
+
+    renderJudgeInfo(){
+        //Don't need to show the judge - user is the judge!
+    }
+
+    renderParticipantsInfo(participants){
+        return (
+            <div>
+                {super.renderParticipantsInfo(participants)}
+                <button>Invite</button>
+            </div>
+        )
+    }
+
+    renderParticipant(participant, number){
+        return <div key={number}>
+            {super.renderParticipant(participant)}
+            <button>-</button>
+        </div>
+    }
+
+    renderLoaded(){
+        return (
+            <div>
+                {super.renderLoaded()}
+                <ParticipantsInviteScreen />
+            </div>
+        )
+    }
+
+}
+
+export default JudgeMatchInfo;
