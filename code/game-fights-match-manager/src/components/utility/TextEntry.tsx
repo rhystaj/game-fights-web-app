c8b22277433b7a14ed6c4ecbf17b17646f8aaa7a
rhystaj <rhystaj@gmail.com>
2020-04-12 14:35:00 +1200

    Convert TextEntry to TSX.

---
+++ code/game-fights-match-manager/src/components/utility/TextEntry.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+type TextEntryProps = {
+    onConfirmEntry: (currentValue: string) => () => void
+    onCancelEntry: () => void;
+}
+
+/**
+ * [DES/PRE] A component in which you can enter text and either confirm or cancel your progress.
+ */
+export default class TextEntry extends Component<TextEntryProps>{
+    
+    currentValue = "";
+
+    render(){
+        return(
+            <div>
+                <input 
+                    type="textedit" 
+                    onInput={(e: React.FormEvent<HTMLInputElement>) => { this.currentValue = e.currentTarget.value }}
+                />
+                <button onClick={() => {this.props.onConfirmEntry(this.currentValue)}}>+</button>
+                <button onClick={this.props.onCancelEntry}>X</button>
+            </div>
+        )
+    }
+    
+}
