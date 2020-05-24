01ba5c2df4449d891e8b5dc472e7e19f5d01d8fd
rhystaj <rhystaj@gmail.com>
2020-05-25 08:57:24 +1200

    Add abilty to assign class to a class to a css list.

--- code/game-fights-match-manager/src/components/utility/ItemList.tsx
+++ code/game-fights-match-manager/src/components/utility/ItemList.tsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { whenUnassigned } from '../../utility/functions/qolFunctions';
 
 export interface ItemListProps<T>{
+    itemListTypeName: string,
     items: T[]
 }
 
@@ -24,7 +25,11 @@ export default class ItemList<T, P extends ItemListProps<T>> extends Component<P
     }
     
     render(){
-        return whenUnassigned(this.props.items, []).map((item: T) => this.renderItem(item));
+        return (
+            <div className={"itemList " + this.props.itemListTypeName}>
+                {whenUnassigned(this.props.items, []).map((item: T) => this.renderItem(item))}
+            </div>
+        );
     }
 
 }
