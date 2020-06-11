c4968bb2fae963f633f72eaa9752fd84e25f0497
rhystaj <rhystaj@gmail.com>
2020-06-12 10:00:53 +1200

    Abstract out AnswerSubmissionComponent from AnswerSubmissionManager as set it as its parent.

---
+++ code/game-fights-match-manager/src/components/match_management/answer_submission/manager/AnswerSubmissionComponent.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import { AnswerSubmissionData } from '../../../../types/datatypes';
+
+import SimpleStateLoadingComponent from '../../../utility/SimpleStateLoadingComponent';
+
+import { GameFightsDataInterfaceManager } from '../../../../backend_interface/game_fights_data_interface/GameFightsDataInterfaceManager';
+import IAnswerSubmissionDataInterface from '../../../../backend_interface/game_fights_data_interface/data_interfaces/IAnswerSubmissionDataInterface';
+
+/**
+ * [DES/PRE] Displays and allows the user to edit anwers to questions.
+ */
+export default abstract class AnswerSubmissionComponent extends 
+  SimpleStateLoadingComponent<GameFightsDataInterfaceManager, AnswerSubmissionData[], IAnswerSubmissionDataInterface>{
+  
+  protected determineComponentClassString(){
+    return super.determineComponentClassString() + " answerSubmissionManager";
+  }
+
+  protected getDataInterface(): IAnswerSubmissionDataInterface{
+    return this.props.dataInterfaceManager.answerSubmissionsInterface;
+  }
+  
+  protected determineInitalData(): AnswerSubmissionData[] {
+    return [];
+  }
+
+  /**
+   * Display the data for an answer submission.
+   * @param answerSubmission The data for the answer submission to shown.
+   */
+  protected abstract renderAnswerSubmission(answerSubmission: AnswerSubmissionData): JSX.Element;
+
+  renderLoaded(dataInterface: IAnswerSubmissionDataInterface, submissions: AnswerSubmissionData[]){
+    return (
+      <div>
+        <h2>My Questions</h2>
+        {submissions.map(submission => (this.renderAnswerSubmission(submission)))}
+        <button className="forfietButton">Forfiet</button>
+      </div>
+    )
+  }
+
+}
--- code/game-fights-match-manager/src/components/match_management/answer_submission/manager/AnswerSubmissionManager.tsx
+++ code/game-fights-match-manager/src/components/match_management/answer_submission/manager/AnswerSubmissionManager.tsx
@@ -4,47 +4,27 @@ import AnswerSubmission from './AnswerSubmission'
 
 import { AnswerSubmissionData } from '../../../../types/datatypes';
 
-import SimpleStateLoadingComponent from '../../../utility/SimpleStateLoadingComponent';
 import SubmissionOptionAction from '../../../../actions/SubmissionOptionActions/SubmissionOptionAction';
 
-import { GameFightsDataInterfaceManager } from '../../../../backend_interface/game_fights_data_interface/GameFightsDataInterfaceManager';
-import IAnswerSubmissionDataInterface from '../../../../backend_interface/game_fights_data_interface/data_interfaces/IAnswerSubmissionDataInterface';
+import AnswerSubmissionComponent from './AnswerSubmissionComponent';
 
 /**
  * [DES/PRE] Displays and allows the user to edit anwers to questions.
  */
-export default class AnswerSubmissionManager extends 
-  SimpleStateLoadingComponent<GameFightsDataInterfaceManager, AnswerSubmissionData[], IAnswerSubmissionDataInterface>{
+export default class AnswerSubmissionManager extends AnswerSubmissionComponent {
   
-  protected determineComponentClassString(){
-    return super.determineComponentClassString() + " answerSubmissionManager";
-  }
-
-  protected getDataInterface(): IAnswerSubmissionDataInterface{
-    return this.props.dataInterfaceManager.answerSubmissionsInterface;
-  }
-  
-  protected determineInitalData(): AnswerSubmissionData[] {
-    return [];
-  }
-
   private onSubmissionOptionAction = async (action: SubmissionOptionAction) => {
     await action.execute(this.getDataInterface());
   }
 
-  renderLoaded(dataInterface: IAnswerSubmissionDataInterface, submissions: AnswerSubmissionData[]){
+  protected renderAnswerSubmission(answerSubmission: AnswerSubmissionData): JSX.Element {
     return (
-      <div>
-        <h2>My Questions</h2>
-        {submissions.map(submission => (
-          <AnswerSubmission
-            key={submission.id}
-            submission={submission}
-            onSubmissionOptionAction={this.onSubmissionOptionAction}
-          />
-        ))}
-        <button className="forfietButton">Forfiet</button>
-      </div>
-    )
+      <AnswerSubmission
+        key={answerSubmission.id}
+        submission={answerSubmission}
+        onSubmissionOptionAction={this.onSubmissionOptionAction}
+      />
+    );
   }
+
 }
