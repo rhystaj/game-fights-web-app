ea42d1b6256067880dbc7adecb4a248fb6c4b2dd
rhystaj <rhystaj@gmail.com>
2020-08-01 14:45:51 +1200

    Give status enums explicit number values.

--- code/game-fights-match-manager/src/enums/statusEnums.ts
+++ code/game-fights-match-manager/src/enums/statusEnums.ts
@@ -1,28 +1,28 @@
 export enum MatchStage {
-    DETERMINING_QUESTIONS,
-    ANSWERS_OPENED,
-    RECORDING_RESULTS
+    DETERMINING_QUESTIONS = 1,
+    ANSWERS_OPENED = 2,
+    RECORDING_RESULTS = 3
 }
 
 export enum AnswerSubmissionState {
-    NO_ANSWER,
-    AWAITING_VALIDATION,
-    PENDING_JUDGE_APPROVAL,
-    ACCEPTED,
-    DECLINED
+    NO_ANSWER = 0,
+    AWAITING_VALIDATION = 1,
+    PENDING_JUDGE_APPROVAL = 2,
+    ACCEPTED = 3,
+    DECLINED = 4
 }
 
 export enum UserMatchStatus{
-    NONE,
-    INVITED,
-    PARTCIPATING,
-    JUDGING
+    NONE = 0,
+    INVITED = 1,
+    PARTCIPATING = 2,
+    JUDGING = 3
 }
 
 export enum FighterMatchStatus{
-    AVAILABLE,
-    ENGAGED,
-    INVITED,
-    PARTCIPATING,
-    DECLINED
+    AVAILABLE = 0,
+    ENGAGED = 1,
+    INVITED = 2,
+    PARTCIPATING = 3,
+    DECLINED = 4
 }
