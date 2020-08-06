eb76567f35ec945d826ffa77f39ff2327a522f3f
rhystaj <rhystaj@gmail.com>
2020-08-07 11:20:29 +1200

    Update future DB implementation of the MatchManager to reflect changes to the interface.

--- GameFightsMatchManagerServer/MatchManager/DBMatchManager.cs
+++ GameFightsMatchManagerServer/MatchManager/DBMatchManager.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Collections.Generic;
 
 namespace MatchManager
 {
+
+    /// <summary>
+    /// A Match Manager that retrieves match data stored in a database.
+    /// </summary>
     public class DBMatchManager : IMatchManager
     {
 
         public DBMatchManager()
         {
 
+        }
+
+        public IMatch Match => throw new System.NotImplementedException();
+
+        public IFighter User => throw new System.NotImplementedException();
+
+        public IEnumerable<IMatchData> GetInvitationsForUser(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IMatch LoadMatchForUser(string userId)
+        {
+            throw new System.NotImplementedException();
         }
 
     }
