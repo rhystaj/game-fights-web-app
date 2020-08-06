e299215d69d94f52e02da1800bcb7e1b05af8ad9
rhystaj <rhystaj@gmail.com>
2020-08-07 11:17:59 +1200

    Create MockMatch.cs

---
+++ GameFightsMatchManagerServer/MatchManager/Mocks/MockMatch.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchManager.Mocks
+{
+    public class MockMatch : IMatch
+    {
+        public IMatchStatus Status => new MatchStatus(UserMatchStatus.JUDGING, MatchStage.ANSWERS_OPENED);
+
+    }
+
+}
