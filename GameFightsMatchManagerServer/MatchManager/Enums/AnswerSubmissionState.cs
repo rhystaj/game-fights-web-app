6bdb23dd1c2f63f3a9d0a5283ba658ce694fefc1
rhystaj <rhystaj@gmail.com>
2020-08-10 10:18:14 +1200

    Create AnswerSubmissionState.cs
    Create enum for AnswerSubmissionState on the backend.

---
+++ GameFightsMatchManagerServer/MatchManager/Enums/AnswerSubmissionState.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchManager.Enums
+{
+    public enum AnswerSubmissionState
+    {
+        NO_ANSWER = 0,
+        AWAITING_VALIDATION = 1,
+        PENDING_JUDGE_APPROVAL = 2,
+        ACCEPTED = 3,
+        DECLINED = 4
+    }
+
+}
