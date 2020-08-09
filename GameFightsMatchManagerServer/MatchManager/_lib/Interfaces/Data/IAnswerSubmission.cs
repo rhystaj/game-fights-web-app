5ed7437114a7df62908859f29643a045da0f854e
rhystaj <rhystaj@gmail.com>
2020-08-10 10:27:50 +1200

    Create IAnswerSubmission.cs

---
+++ GameFightsMatchManagerServer/MatchManager/_lib/Interfaces/Data/IAnswerSubmission.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MatchManager
+{
+
+    /// <summary>
+    /// A submission for an answer a question.
+    /// </summary>
+    public interface IAnswerSubmission
+    {
+
+        /// <summary>
+        /// The question the answer in the submission is answering.
+        /// </summary>
+        [JsonPropertyName("question")] string Question { get; }
+
+        /// <summary>
+        /// The answer to the question.
+        /// </summary>
+        [JsonPropertyName("answer")] string Answer { get; }
+
+        /// <summary>
+        /// The state of the answer submission in relation to the match.
+        /// </summary>
+        [JsonPropertyName("state")] AnswerSubmissionState State { get; }
+
+        /// <summary>
+        /// Whether the answer has been validated.
+        /// </summary>
+        [JsonPropertyName("validatedByUser")] bool ValidatedByUser { get; }
+
+    }
+
+}
